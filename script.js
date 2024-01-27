933dfa8da23feb9c9a6681467493733f746ec19b
muneebwaqas416 <muneebwaqas416@gmail.com>
2024-01-28 02:35:07 +0500

    Commit_new

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 var scriptTag = document.currentScript;
 var parameter = scriptTag.getAttribute('data-parameter');
 
-var element = document.getElementByClassName('getelement')
-element.innerText = `This is the value passed as paramter ${parameter}`
 
 // Now you can use the 'parameter' variable in this script
 console.log("Parameter in encrypt.js:", parameter);
